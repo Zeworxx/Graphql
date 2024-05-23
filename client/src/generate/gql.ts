ce161e599e228c8577e2d2bd35f4de7f14d4cc09
Amir Talbi <amirotalbi94@gmail.com>
2024-05-23 19:25:06 +0200

    feat: pushing branch

---
+++ client/src/generate/gql.ts
@@ -0,0 +1,60 @@
+/* eslint-disable */
+import * as types from './graphql';
+import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+
+/**
+ * Map of all GraphQL operations in the project.
+ *
+ * This map has several performance disadvantages:
+ * 1. It is not tree-shakeable, so it will include all operations in the project.
+ * 2. It is not minifiable, so the string of a GraphQL query will be multiple times inside the bundle.
+ * 3. It does not support dead code elimination, so it will add unused operations.
+ *
+ * Therefore it is highly recommended to use the babel or swc plugin for production.
+ */
+const documents = {
+<<<<<<< HEAD:client/src/graphql/gql.ts
+    "mutation signIn($username: String!, $password: String!) {\n  signIn(username: $username, password: $password) {\n    token\n    userId\n    code\n    message\n    success\n  }\n}": types.SignInDocument,
+    "mutation CreateUser($username: String!, $password: String!) {\n                    createUser(username: $username, password: $password) {\n                        code\n                        success\n                        message\n                        user {\n                        id\n                        username\n                        }\n                    }\n                }": types.CreateUserDocument,
+    "\n  mutation CreateArticle($content: String!, $userId: ID!) {\n    createArticle(content: $content, userId: $userId) {\n      message\n      code\n      success\n    }\n  }\n": types.CreateArticleDocument,
+=======
+    "mutation CreateUser($username: String!, $password: String!) {\n                    createUser(username: $username, password: $password) {\n                        success\n                        message\n                        user {\n                        id\n                        username\n                        }\n                    }\n                }": types.CreateUserDocument,
+>>>>>>> 2256587 (feat: pushing branch):client/src/generated/gql.ts
+};
+
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ *
+ *
+ * @example
+ * ```ts
+ * const query = graphql(`query GetUser($id: ID!) { user(id: $id) { name } }`);
+ * ```
+ *
+ * The query argument is unknown!
+ * Please regenerate the types.
+ */
+export function graphql(source: string): unknown;
+
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+<<<<<<< HEAD:client/src/graphql/gql.ts
+export function graphql(source: "mutation signIn($username: String!, $password: String!) {\n  signIn(username: $username, password: $password) {\n    token\n    userId\n    code\n    message\n    success\n  }\n}"): (typeof documents)["mutation signIn($username: String!, $password: String!) {\n  signIn(username: $username, password: $password) {\n    token\n    userId\n    code\n    message\n    success\n  }\n}"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "mutation CreateUser($username: String!, $password: String!) {\n                    createUser(username: $username, password: $password) {\n                        code\n                        success\n                        message\n                        user {\n                        id\n                        username\n                        }\n                    }\n                }"): (typeof documents)["mutation CreateUser($username: String!, $password: String!) {\n                    createUser(username: $username, password: $password) {\n                        code\n                        success\n                        message\n                        user {\n                        id\n                        username\n                        }\n                    }\n                }"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  mutation CreateArticle($content: String!, $userId: ID!) {\n    createArticle(content: $content, userId: $userId) {\n      message\n      code\n      success\n    }\n  }\n"): (typeof documents)["\n  mutation CreateArticle($content: String!, $userId: ID!) {\n    createArticle(content: $content, userId: $userId) {\n      message\n      code\n      success\n    }\n  }\n"];
+=======
+export function graphql(source: "mutation CreateUser($username: String!, $password: String!) {\n                    createUser(username: $username, password: $password) {\n                        success\n                        message\n                        user {\n                        id\n                        username\n                        }\n                    }\n                }"): (typeof documents)["mutation CreateUser($username: String!, $password: String!) {\n                    createUser(username: $username, password: $password) {\n                        success\n                        message\n                        user {\n                        id\n                        username\n                        }\n                    }\n                }"];
+>>>>>>> 2256587 (feat: pushing branch):client/src/generated/gql.ts
+
+export function graphql(source: string) {
+  return (documents as any)[source] ?? {};
+}
+
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
--- client/src/views/IndexVue.vue
+++ client/src/views/IndexVue.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< Updated upstream
     <navbar />
     <post />
 </template>
@@ -12,43 +11,4 @@ export default {
         Post
     }
 }
-=======
-  <div>
-    <h1>Welcome to My Social Network</h1>
-    <div v-if="loading">Loading...</div>
-    <div v-else>
-      <Post v-for="post in posts" :key="post.id" :post="post" />
-    </div>
-  </div>
-</template>
-
-<script setup lang="ts">
-import { useQuery } from '@apollo/client';
-import Post from '@/components/Post.vue';
-import { defineComponent } from 'vue';
-
-defineComponent({
-  components: {
-    Post,
-  },
-});
-
-// const GET_POSTS = gql`
-//   query GetArticles {
-//     posts {
-//       id
-//       title
-//       author
-//       content
-//       image
-//       likes
-//       comments
-//     }
-//   }
-// `;
-
-// const { loading, data } = useQuery(GET_POSTS);
-// const posts = data?.posts || [];
-
->>>>>>> Stashed changes
 </script>

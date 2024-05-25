241ffdf70fd858ff88ed94aae1a183a7647d04b5
Jibe-shoptaprod <jeanbaptiste.omilus@gmail.com>
2024-05-25 20:48:52 +0200

    feet: Update fontawesome imports and add editing functionality to post view

--- client/src/fontawesome.ts
+++ client/src/fontawesome.ts
@@ -1,5 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUser,faArrowRight,faEnvelope,faLock ,faThumbsUp, faThumbsDown, faCommentAlt} from '@fortawesome/free-solid-svg-icons'
+import { faUser,faArrowRight,faEnvelope,faLock ,faThumbsUp, faThumbsDown, faCommentAlt , faPenToSquare,faTrash} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faUser)
@@ -9,5 +9,7 @@ library.add(faLock)
 library.add(faThumbsUp)
 library.add(faThumbsDown)
 library.add(faCommentAlt)
+library.add(faPenToSquare)
+library.add(faTrash)
 
 export { FontAwesomeIcon }
